e75d10828085cbb8124643f16a8c57209b30dcd7
1000ch <shogo.sensui@gmail.com>
2016-01-13 10:07:03 +0900

    use es2015 syntax

--- src/js/background.js
+++ src/js/background.js
@@ -1,56 +1,49 @@
 // launch options.html on installation
-chrome.runtime.onInstalled.addListener(function (details) {
+chrome.runtime.onInstalled.addListener(details => {
   if (details.reason == 'install'){
     chrome.tabs.create({'url': '/html/options.html'});
   }
 });
 
 // open urls on icon clicked
-chrome.browserAction.onClicked.addListener(function () {
+chrome.browserAction.onClicked.addListener(() => {
+  const chromeStorage = chrome.storage.sync;
+  const dataKey = 'urlopener_data';
+  const protocol = /^[^:]+(?=:\/\/)/;
 
-  var chromeStorage = chrome.storage.sync;
-  var dataKey = 'urlopener_data';
-  var protocol = /^[^:]+(?=:\/\/)/;
-
-  chromeStorage.get(dataKey, function(items) {
-
-    var array = items[dataKey];
+  chromeStorage.get(dataKey, items => {
+    let array = items[dataKey];
 
     if (!Array.isArray(array)) {
       array = [];
     }
 
-    chrome.tabs.query({}, function (tabs) {
+    chrome.tabs.query({}, tabs => {
+      let openedTabs = {};
+      let openedUrls = [];
 
-      var openedTabs = {};
-      var openedUrls = [];
-
-      tabs.forEach(function (tab) {
-        var url = tab.url.replace(protocol, '');
+      tabs.forEach(tab => {
+        let url = tab.url.replace(protocol, '');
         openedTabs[url] = tab.id;
         openedUrls.push(url);
       });
 
-      array.forEach(function (item) {
+      array.forEach(item => {
+        let url = item.url.replace(protocol, '');
+        let target;
 
-        var url = item.url.replace(protocol, '');
-        var target;
-
-        if (openedUrls.some(function (openedUrl) {
-
+        if (openedUrls.some(openedUrl => {
           if (openedUrl.indexOf(url) !== -1) {
             target = openedUrl;
             return true;
           } else {
             return false;
           }
-
         })) {
           //　item.url is already opened
           console.log(target, item.index);
 
-          var tabId = openedTabs[target];
-
+          let tabId = openedTabs[target];
           chrome.tabs.update(tabId, {
             pinned: item.pinned
           });
@@ -60,25 +53,19 @@ chrome.browserAction.onClicked.addListener(function () {
               index: item.index
             });
           }
-
         } else {
           // item.url is not opened yet
-
           if (Number.isInteger(item.index)) {
-
             chrome.tabs.create({
               url: item.url,
               pinned: item.pinned,
               index: item.index
             });
-
           } else {
-
             chrome.tabs.create({
               url: item.url,
               pinned: item.pinned
             });
-
           }
 
           openedUrls.push(item.url.replace(protocol, ''));
--- src/js/options.js
+++ src/js/options.js
@@ -1,40 +1,38 @@
-$(function () {
+$(() => {
+  const chromeStorage = chrome.storage.sync;
+  const dataKey = 'urlopener_data';
+  let data = [];
 
-  var chromeStorage = chrome.storage.sync;
-  var dataKey = 'urlopener_data';
-  var data = [];
-
-  var $url = $('#js-url');
-  var $pinned = $('#js-pinned');
-  var $save = $('#js-save');
-  var $form = $('#js-form');
-  var $urls = $('#js-urls');
+  const $url = $('#js-url');
+  const $pinned = $('#js-pinned');
+  const $save = $('#js-save');
+  const $form = $('#js-form');
+  const $urls = $('#js-urls');
 
   new Slip($urls.get(0));
 
-  $urls.on('slip:swipe', function(e) {
+  $urls.on('slip:swipe', e => {
+    const $li = $(e.originalEvent.target);
+    const url = $li.attr('data-url');
 
-    var $li = $(e.originalEvent.target);
-    var url = $li.attr('data-url');
+    data = data.filter(item => item.url !== url);
 
-    data = data.filter(function (item) {
-      return (item.url !== url);
-    });
-
-    chromeStorage.set({'urlopener_data': data}, function () {
+    chromeStorage.set({'urlopener_data': data}, () => {
       e.target.parentNode.removeChild(e.originalEvent.target);
     });
   });
 
-  $urls.on('slip:reorder', function(e) {
+  $urls.on('slip:reorder', e => {
+    e.target.parentNode.insertBefore(
+      e.originalEvent.target,
+      e.originalEvent.detail.insertBefore
+    );
 
-    e.target.parentNode.insertBefore(e.originalEvent.target, e.originalEvent.detail.insertBefore);
+    const url = $(e.originalEvent.target).attr('data-url');
+    const order = e.originalEvent.detail.spliceIndex;
+    const $list = $urls.find('li');
 
-    var url = $(e.originalEvent.target).attr('data-url');
-    var order = e.originalEvent.detail.spliceIndex;
-    var $list = $urls.find('li');
-
-    data.forEach(function (item) {
+    data.forEach(item => {
       if (url === item.url) {
         item.order = order;
       } else {
@@ -45,12 +43,11 @@ $(function () {
     chromeStorage.set({'urlopener_data': data});
   });
 
-  $save.on('click', function () {
+  $save.on('click', () => {
     $form.trigger('submit');
   });
 
-  $form.on('submit', function (e) {
-
+  $form.on('submit', e => {
     if (!Array.isArray(data)) {
       data = [];
     }
@@ -61,19 +58,18 @@ $(function () {
       order: data.length
     });
 
-    chromeStorage.set({'urlopener_data': data}, function () {
+    chromeStorage.set({'urlopener_data': data}, () => {
       renderItem(data);
       $url.val('');
       $pinned.prop('checked', false);
     });
   });
 
-  $urls.on('click', '.js-pin', function () {
+  $urls.on('click', '.js-pin', e => {
+    const $li = $(e.originalEvent.target).toggleClass('is-pinned').closest('li');
+    const url = $li.attr('data-url');
 
-    var $li = $(this).toggleClass('is-pinned').closest('li');
-    var url = $li.attr('data-url');
-
-    data.forEach(function (item) {
+    data.forEach(item => {
       if (url === item.url) {
         item.pinned = !item.pinned;
       }
@@ -82,28 +78,22 @@ $(function () {
     chromeStorage.set({'urlopener_data': data});
   });
 
-  $urls.on('click', '.js-del', function () {
-    var $li = $(this).closest('li');
-    var url = $li.attr('data-url');
+  $urls.on('click', '.js-del', e => {
+    const $li = $(e.originalEvent.target).closest('li');
+    const url = $li.attr('data-url');
 
-    data = data.filter(function (item) {
-      return (item.url !== url);
-    });
-
-    chromeStorage.set({'urlopener_data': data}, function () {
+    data = data.filter(item => item.url !== url);
+    chromeStorage.set({'urlopener_data': data}, () => {
       $li.remove();
     });
   });
 
   function renderItem(items) {
+    const list = [];
 
-    items = items.sort(function (a, b) {
-      return (a.order || 0) > (b.order || 0);
-    });
-
-    var list = [];
-    items.forEach(function (item) {
-      var li =
+    items = items.sort((a, b) => (a.order || 0) > (b.order || 0));
+    items.forEach(item => {
+      const li =
         '<li class="list-group-item" data-url="' + item.url + '">' +
           '<span class="js-pin glyphicon glyphicon-pushpin' + (item.pinned ? ' is-pinned' : '') + '"></span>' +
           '<a style="margin-left: 10px;" href="' + item.url + '">' + item.url + '</a>' +
@@ -115,14 +105,11 @@ $(function () {
     $urls.html(list);
   }
 
-  chromeStorage.get(dataKey, function(items) {
-
+  chromeStorage.get(dataKey, items => {
     data = items[dataKey];
-
     if (!Array.isArray(data)) {
       data = [];
     }
-
     renderItem(data);
   });
 });
