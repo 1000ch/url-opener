25540e8e314da34e592556ce3890429899018a0a
1000ch <shogo.sensui@gmail.com>
2016-01-13 10:06:37 +0900

    refresh build flow

---
+++ Makefile
@@ -0,0 +1,27 @@
+PROJECTNAME="URL opener"
+
+all: prelogue build archive epilogue
+
+prelogue:
+	@echo ""
+	@echo ">>> $(PROJECTNAME) build started"
+	@echo ""
+
+node_modules: package.json
+	@npm install
+
+bower_components: bower.json
+	@bower install
+
+build: node_modules bower_components
+	@gulp build
+
+archive: ./dist
+	@zip url-opener.zip -r ./dist
+
+epilogue:
+	@echo ""
+	@echo ">>> $(PROJECTNAME) build has successfully finished"
+	@echo ""
+
+.PHONY: prelogue build archive epilogue
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,18 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 
-gulp.task('copy', function () {
-  
-  gulp.src('bower_components/bootstrap/dist/fonts/*').pipe(gulp.dest('src/fonts'));
-  gulp.src('bower_components/bootstrap/dist/js/bootstrap.min.js').pipe(gulp.dest('src/js'));
+gulp.task('copy', () => {
+  gulp.src('bower_components/bootstrap/dist/fonts/*').pipe(gulp.dest('dist/fonts'));
   gulp.src('bower_components/bootstrap/dist/js/bootstrap.min.js').pipe(gulp.dest('dist/js'));
-  gulp.src('bower_components/jquery/dist/jquery.min.js').pipe(gulp.dest('src/js'));
   gulp.src('bower_components/jquery/dist/jquery.min.js').pipe(gulp.dest('dist/js'));
-  gulp.src('bower_components/slip/slip.js').pipe(gulp.dest('src/js'));
   gulp.src('bower_components/slip/slip.js').pipe(gulp.dest('dist/js'));
   gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.css').pipe(gulp.dest('dist/css'));
-
+  gulp.src('src/icons/*').pipe(gulp.dest('dist/icons'));
+  gulp.src('src/manifest.json').pipe(gulp.dest('dist'));
 });
 
-gulp.task('html', function () {
-  
-  var htmlmin = require('gulp-htmlmin');
-  
+gulp.task('html', () => {
+  const htmlmin = require('gulp-htmlmin');
+
   return gulp.src('src/html/*.html')
     .pipe(htmlmin({
       collapseWhitespace: true
@@ -24,12 +20,11 @@ gulp.task('html', function () {
     .pipe(gulp.dest('dist/html'))
 });
 
-gulp.task('css', function () {
+gulp.task('css', () => {
+  const uncss   = require('gulp-uncss');
+  const csscomb = require('gulp-csscomb');
+  const csso    = require('gulp-csso');
 
-  var uncss   = require('gulp-uncss');
-  var csscomb = require('gulp-csscomb');
-  var csso    = require('gulp-csso');
-  
   return gulp.src('src/css/bootstrap.min.css')
     .pipe(uncss({
       html: ['src/html/options.html']
@@ -39,15 +34,18 @@ gulp.task('css', function () {
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('js', function () {
-  
-  var uglify = require('gulp-uglify');
-  
+gulp.task('js', () => {
+  const babel = require('gulp-babel');
+  const uglify = require('gulp-uglify');
+
   return gulp.src('src/js/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('build', function () {
+gulp.task('build', () => {
   gulp.start('copy', 'html', 'css', 'js');
 });
